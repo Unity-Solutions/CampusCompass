1bc8e96c34990691800a74c1517549fea123371b
qwerty <daniil.pyatiletkin@gmail.com>
2024-07-18 21:56:54 +0200

    main-page mokap

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import {MainPageComponent} from "./app/application/components/main-page/main-page.component";
+import {AppComponent} from "./app/app.component";
+
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
