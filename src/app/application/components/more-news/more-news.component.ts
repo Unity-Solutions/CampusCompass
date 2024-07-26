3d289a5d10e437305e40f5a561b553ad919935ed
qwerty <daniil.pyatiletkin@gmail.com>
2024-07-26 15:44:10 +0200

    news page mockup

---
+++ src/app/application/components/more-news/more-news.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-more-news',
+  standalone: true,
+  imports: [],
+  templateUrl: './more-news.component.html',
+  styleUrl: './more-news.component.scss'
+})
+export class MoreNewsComponent {
+
+}
--- src/app/application/pages/news-page/news-page.component.ts
+++ src/app/application/pages/news-page/news-page.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import {NavComponent} from "../../../shared/";
+import {InfoComponent, NavComponent} from "../../../shared/";
 import {PersNewsComponent} from "../../components/pers-news/pers-news.component";
+import {NewsMainComponent} from "../../components/news-main/news-main.component";
+import {MoreNewsComponent} from "../../components/more-news/more-news.component";
 
 @Component({
   selector: 'app-news-page',
   standalone: true,
   imports: [
     NavComponent,
-    PersNewsComponent
+    PersNewsComponent,
+    NewsMainComponent,
+    MoreNewsComponent,
+    InfoComponent
   ],
   templateUrl: './news-page.component.html',
   styleUrl: './news-page.component.scss'
