952a0e7560824c55ef354267fcc7707a28ba5a95
DanyaPyat <daniil.pyatiletkin@gmail.com>
2024-09-11 18:07:11 +0200

    login service created + agitation button

--- src/app/application/modules/news-page/pers-news/pers-news.component.ts
+++ src/app/application/modules/news-page/pers-news/pers-news.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
+import {NgIf} from "@angular/common";
+import {AuthService} from "../../../services/auth.service";
 
 @Component({
   selector: 'app-pers-news',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './pers-news.component.html',
   styleUrl: './pers-news.component.scss'
 })
 export class PersNewsComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, public authService: AuthService) {}
+
   redirectToLogin(): void {
     this.router.navigate(['/login']);
   }
+
 
 
 }
