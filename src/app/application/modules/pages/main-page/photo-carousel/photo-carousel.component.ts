4187f675b9bbd89ab50a41e713908689bdaa873e
DanyaPyat <daniil.pyatiletkin@gmail.com>
2024-10-02 13:27:10 +0200

    edited

--- src/app/application/modules/pages/main-page/photo-carousel/photo-carousel.component.ts
+++ src/app/application/modules/pages/main-page/photo-carousel/photo-carousel.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./photo-carousel.component.scss'],
 })
 export class PhotoCarouselComponent implements OnInit {
-  slides: any[] = new Array(3).fill({
+  public arr_length: number = 5
+
+  slides: any[] = new Array(this.arr_length).fill({
     id: -1,
     src: '',
     title: '',
@@ -23,5 +25,13 @@ export class PhotoCarouselComponent implements OnInit {
     this.slides[2] = {
       src: '/img/carousel/pexels-cottonbro-news.png',
     }
+    this.slides[3] = {
+      src: '/img/carousel/andrew-neel-young-woman-pc.png',
+    }
+    this.slides[4] = {
+      src: '/img/carousel/pexels-cottonbro-news.png',
+    }
   }
+
+  protected readonly Math = Math
 }
