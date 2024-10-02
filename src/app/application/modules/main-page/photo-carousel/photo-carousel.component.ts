594a7fd2e9d94e9bed867d4be4bdcace2cdec11e
Snol-std <3.com333333333@gmail.com>
2024-10-02 10:44:46 +0300

    edited

--- src/app/application/modules/main-page/photo-carousel/photo-carousel.component.ts
+++ src/app/application/modules/main-page/photo-carousel/photo-carousel.component.ts
@@ -1,30 +1,87 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
+// import { Component, OnInit, Renderer2 } from '@angular/core';
+// // import { CarouselModule } from 'ngx-bootstrap/carousel';
+
+// @Component({
+//   selector: 'app-photo-carousel',
+//   standalone: true,
+//   imports: [
+//   ],
+//   templateUrl: './photo-carousel.component.html',
+//   styleUrls: ['./photo-carousel.component.scss']
+// })
+
+// export class PhotoCarouselComponent implements OnInit {
+
+//   // constructor(private renderer: Renderer2) {}
+
+//   ngOnInit(): void {
+    
+//   }
+
+
+// }
+// import { Component, OnInit } from '@angular/core';
+// import { NgFor } from '@angular/common';
+// import { CarouselComponent, CarouselInnerComponent, CarouselItemComponent } from '@coreui/angular';
+// // import { ThemeDirective } from '@coreui/angular'
+
+// @Component({
+//   selector: 'app-photo-carousel',
+//   templateUrl: './photo-carousel.component.html',
+//   styleUrls: ['./photo-carousel.component.scss'],
+//   standalone: true,
+//   imports: [CarouselComponent, CarouselInnerComponent, NgFor, CarouselItemComponent],
+//   // ThemeDirective, 
+// })
+// export class PhotoCarouselComponent implements OnInit {
+
+//   slides: any[] = new Array(3).fill({ id: -1, src: '', title: '', subtitle: '' });
+
+//   ngOnInit(): void {
+//     this.slides[0] = {
+//       src: './assets/img/angular.jpg'
+//     };
+//     this.slides[1] = {
+//       src: './assets/img/react.jpg'
+//     };
+//     this.slides[2] = {
+//       src: './assets/img/vue.jpg'
+//     };
+//   }
+
+//   onItemChange($event: any): void {
+//     console.log('Carousel onItemChange', $event);
+//   }
+// }
+
+import { Component, OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { CarouselComponent, CarouselInnerComponent, CarouselItemComponent} from '@coreui/angular';
 
 @Component({
   selector: 'app-photo-carousel',
-  standalone: true,
-  imports: [
-    CarouselModule
-  ],
   templateUrl: './photo-carousel.component.html',
-  styleUrls: ['./photo-carousel.component.scss']
+  styleUrls: ['./photo-carousel.component.scss'],
+  standalone: true,
+  imports: [CarouselComponent, CarouselInnerComponent, NgFor, CarouselItemComponent]
 })
-
 export class PhotoCarouselComponent implements OnInit {
 
-  constructor(private renderer: Renderer2) {}
+  slides: any[] = new Array(3).fill({ id: -1, src: '', title: '', subtitle: '' });
 
   ngOnInit(): void {
-    this.loadScript('https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js');
-    this.loadScript('https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js');
+    this.slides[0] = {
+      src: './assets/img/angular.jpg'
+    };
+    this.slides[1] = {
+      src: './assets/img/react.jpg'
+    };
+    this.slides[2] = {
+      src: './assets/img/vue.jpg'
+    };
   }
 
-  loadScript(src: string): void {
-    const script = this.renderer.createElement('script');
-    script.src = src;
-    script.type = 'text/javascript';
-    script.async = true;
-    this.renderer.appendChild(document.body, script);
+  onItemChange($event: any): void {
+    console.log('Carousel onItemChange', $event);
   }
 }
