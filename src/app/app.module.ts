38c0affac752ae08f5e12c5ec2c671c3ef6a6f03
DanyaPyat <daniil.pyatiletkin@gmail.com>
2024-10-09 22:05:34 +0200

    nav changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { routes } from './app.routes'
 import { AppComponent } from './app.component'
 import { BrowserModule } from '@angular/platform-browser'
 import { ApplicationModule } from './application/application.module'
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [RouterModule.forRoot(routes), ApplicationModule, BrowserModule],
+  imports: [
+    RouterModule.forRoot(routes),
+    ApplicationModule,
+    BrowserModule,
+    SharedModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
