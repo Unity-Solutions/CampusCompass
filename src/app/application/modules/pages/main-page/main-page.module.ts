9ebda788729c8bf6ae723f830d7774eb8f01be4b
DanyaPyat <daniil.pyatiletkin@gmail.com>
2024-11-19 18:37:35 +0100

    slider created

--- src/app/application/modules/pages/main-page/main-page.module.ts
+++ src/app/application/modules/pages/main-page/main-page.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core'
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import {
   MainPageComponent,
@@ -9,7 +9,6 @@ import {
   PartnersComponent,
 } from './'
 import { SharedModule } from '../../../../shared/shared.module'
-
 @NgModule({
   declarations: [
     MainPageComponent,
@@ -28,5 +27,6 @@ import { SharedModule } from '../../../../shared/shared.module'
     MapBlockComponent,
     PartnersComponent,
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class MainPageModule {}
--- src/app/application/modules/pages/main-page/photo-carousel/photo-carousel.component.ts
+++ src/app/application/modules/pages/main-page/photo-carousel/photo-carousel.component.ts
@@ -1,49 +1,27 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, AfterViewInit } from '@angular/core'
+import Swiper from 'swiper'
+import { Autoplay, Navigation } from 'swiper/modules'
 
 @Component({
   selector: 'app-photo-carousel',
   templateUrl: './photo-carousel.component.html',
   styleUrls: ['./photo-carousel.component.scss'],
 })
-export class PhotoCarouselComponent implements OnInit {
-  currentSlide = 0
-  items = [
-    'Find university',
-    'Find job',
-    'Proforientation',
-    'Item 4',
-    'Item 5',
-    'Item 6',
-    'Item 7',
-    'Item 8',
-    'Item 9',
-  ]
-  visibleItems = 3
-
-  constructor() {}
-
-  ngOnInit(): void {}
-
-  prev(): void {
-    this.currentSlide =
-      this.currentSlide === 0
-        ? this.items.length - this.visibleItems
-        : this.currentSlide - this.visibleItems
-    this.updateSlidePosition()
-  }
-
-  next(): void {
-    this.currentSlide =
-      this.currentSlide >= this.items.length - this.visibleItems
-        ? 0
-        : this.currentSlide + this.visibleItems
-    this.updateSlidePosition()
-  }
-
-  updateSlidePosition(): void {
-    const carouselInner = document.querySelector(
-      '.carousel-inner'
-    ) as HTMLElement
-    carouselInner.style.transform = `translateX(-${this.currentSlide * (100 / this.visibleItems)}%)`
+export class PhotoCarouselComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    Swiper.use([Navigation, Autoplay])
+    new Swiper('.mySwiper', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      loop: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    })
   }
 }
