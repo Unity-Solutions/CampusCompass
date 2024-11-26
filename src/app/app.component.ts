4d35d2b5d3a6a17e78cef47813ab0e49313ff9e2
DanyaPyat <daniil.pyatiletkin@gmail.com>
2024-11-26 09:46:18 +0100

    edited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core'
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core'
+import { AddBlockCheckService } from './application/services'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'proj01'
+export class AppComponent implements AfterViewInit {
+  constructor(private addBlockService: AddBlockCheckService) {}
+  public isShown = false
+  ngAfterViewInit() {
+    // Подписываемся на Observable и обновляем isShown
+    this.addBlockService.isAddBlock$.subscribe((status) => {
+      console.log(status)
+      this.isShown = status
+    })
+  }
+
+  // ngAfterViewInit() {
+  //   setTimeout(() => {
+  //     console.log(this.addBlockService.isAddBlock)
+  //     this.isShown = this.addBlockService.isAddBlock
+  //   }, 600)
+  // }
 }
